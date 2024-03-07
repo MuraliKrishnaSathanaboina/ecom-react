f3c4eb66b518214985ec6c12c4e7df922eb8de65
npx <smuralikrishna@lyrostech.com>
2024-03-07 09:26:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import topnav from './topnav';
+import Topnav from './Topnav';
+
 
 function App() {
   return (
     <div >
-      <topnav/>
+      <Topnav/>
     </div>
   );
 }
--- src/topnav.js
+++ src/topnav.js
@@ -1,29 +1,30 @@
 import React from 'react'
-import 'topnav.css'
+import './topnav.css'
+// import basket from './Assests/basket.png';
 
-function topnav() {
-  return (
-    <div>
-        <div class="container1">
-        <p id="para">Fresh Meat</p>
-        {/* <!-- <a onclick="non_veg()">NON-VEG</a> -->
-        <!-- <a onclick="veg1()">NON-VEG</a> --> */}
-
-        <input
-          id="searchbar"
-          onkeyup="search_price()"
-          type="text"
-          name="search"
-          placeholder="Search by price"
-        />
-        <div class="cartImg" onclick="cartt1()">
-          <a>
-            <img src="./photos/basket.png" alt="" height="40px" width="40px" />
-          </a>
+function Topnav() {
+    return (
+        <div>
+            <div class="container1">
+            <p id="para">Fresh Meat</p>
+            {/* <!-- <a onclick="non_veg()">NON-VEG</a> -->
+            <!-- <a onclick="veg1()">NON-VEG</a> --> */}
+    
+            <input
+              id="searchbar"
+              onkeyup="search_price()"
+              type="text"
+              name="search"
+              placeholder="Search by price"
+            />
+            <div class="cartImg" onclick="cartt1()">
+              <a>
+                <img src="./public/Images/basket.png" alt="" height="40px" width="40px" />
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  )
+      )
 }
 
-export default topnav
+export default Topnav
